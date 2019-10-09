cc237c4fffd6958f7a6c9b351b5a894b6c738ff4
Andrii Krytskyi <>
2019-10-09 10:35:12 +0300

    1 commit

---
+++ 1.java
@@ -0,0 +1,12 @@
+public class Test1 {
+	public static void main(String[] args){
+		int a=10;
+		int b=3;
+		int c=7;
+		int result=a+b+c;
+	System.out.println("Sum of "+a+" and "+b+" and "+c+" is: "+result);
+	System.out.println("Hello Word");
+	
+
+	}
+}
