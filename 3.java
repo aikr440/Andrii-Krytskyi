f258bd5d5ce223aebf284c941a4c530eafc63563
Andrii Krytskyi <>
2019-10-09 10:37:28 +0300

    2 commit

---
+++ 3.java
@@ -0,0 +1,7 @@
+public class Test3 {
+    public static void main(String[] args) {
+        int i = 123;
+        System.out.println("Enter a decimal number:" +i);
+        System.out.println(String.format("Decimal to hexadecimal: %X",i));
+    }
+}
