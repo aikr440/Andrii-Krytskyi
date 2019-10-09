69518e5e2a310062d4523e29320495621554f5ba
Andrii Krytskyi <>
2019-10-09 10:40:20 +0300

    1.1 commit

--- 1.java
+++ 1.java
@@ -1,12 +1,12 @@
 public class Test1 {
 	public static void main(String[] args){
-		int a=10;
-		int b=3;
-		int c=7;
+		int a=1;
+		int b=6;
+		int c=4;
 		int result=a+b+c;
 	System.out.println("Sum of "+a+" and "+b+" and "+c+" is: "+result);
-	System.out.println("Hello Word");
-	
+	System.out.println("AAAAAAAAAAAA");
+	////////////////////
 
 	}
 }
